0d45626e1f75e264f9d6831dec7392c1e4950fce
huangyong <huangyongwr@live.com>
2016-04-19 10:29:42 +0800

    just test

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: hello.cpp
+  > Description: 
+	> Author: HuangChunLin
+	> Mail:huangyongwr@live.com
+	> Created Time: Tue 19 Apr 2016 10:29:09 AM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    cout<<" hello friend"<<endl;
+    return 0;
+}
+
